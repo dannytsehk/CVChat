fdff15d267cc3a3843a3a4ee1decb73957108fbd
dannytsehk <wai.tse.diary@gmail.com>
2025-10-25 17:18:26 +0800

    Change app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ if user_input:
     # Send to Poe API
     headers = {
         "Content-Type": "application/json",
-        "Authorization": f"Bearer {POE_API_KEY}"
+        "Authorization": f"Bearer eofj0b29XgNokNgGdUXnef0D-1eSuuAKDeoeZ5Ub_OA"
     }
     payload = {
         "model": "ChatBot_CSS",
